8d12adaa5a5505a5240166da82434d96227b4471
MichaelHabermas <michaelghabermas@gmail.com>
2021-10-27 14:34:56 -0400

    basic footer links formatting

--- src/components/Layout/components/Footer.jsx
+++ src/components/Layout/components/Footer.jsx
@@ -2,11 +2,88 @@ import React from "react";
 
 import HelpShareThePromise from "../../Global/HelpShareThePromise";
 
+import * as styles from "../../../styles/footer.module.css";
+
 export default function Footer() {
-  return (
-    <div>
-      <HelpShareThePromise />
-      <h1>Footer</h1>
-    </div>
-  );
+	return (
+		<div>
+			<HelpShareThePromise />
+			<footer>
+				<div className={styles.container}>
+					<section className={styles.linksContainer}>
+						<div className={styles.linkContainer}>
+							<h3>WHO WE ARE</h3>
+							<div className={styles.links}>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+							</div>
+						</div>
+						<div className={styles.linkContainer}>
+							<h3>WHO WE ARE</h3>
+							<div className={styles.links}>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+							</div>
+						</div>
+						<div className={styles.linkContainer}>
+							<h3>WHO WE ARE</h3>
+							<div className={styles.links}>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+							</div>
+						</div>
+						<div className={styles.linkContainer}>
+							<h3>WHO WE ARE</h3>
+							<div className={styles.links}>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+								<a href="#" alt="altyalt">
+									Mission & Vision
+								</a>
+							</div>
+						</div>
+						<div className={styles.linkContainer}>
+							<button className={styles.donateButton}>Donate</button>
+						</div>
+					</section>
+
+					<section className={styles.container}></section>
+				</div>
+			</footer>
+		</div>
+	);
 }
