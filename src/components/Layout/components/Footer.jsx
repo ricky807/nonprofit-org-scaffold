cecfb0b2388b7056392ebf059ff11eecc5052904
MichaelHabermas <michaelghabermas@gmail.com>
2021-10-28 12:19:54 -0400

    footter links code refactored

--- src/components/Layout/components/Footer.jsx
+++ src/components/Layout/components/Footer.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 
+// components
 import HelpShareThePromise from "../../Global/HelpShareThePromise";
-
+// styling
 import * as styles from "../../../styles/footer.module.css";
+// assets
+import FPLogo from "../../../images/familypromiselogo.png";
+
+// footer data
+const footerData = [
+	{
+		title: "WHO WE ARE",
+		links: [
+			{ linkName: "Mission & Vision", link: "" },
+			{ linkName: "Programs", link: "" },
+			{ linkName: "History", link: "" },
+			{ linkName: "Staff", link: "" },
+			{ linkName: "Board", link: "" },
+			{ linkName: "Financials", link: "" },
+		],
+	},
+	{
+		title: "WHAT WE DO",
+		links: [
+			{ linkName: "Prevent", link: "" },
+			{ linkName: "Provide", link: "" },
+			{ linkName: "Prepare", link: "" },
+		],
+	},
+	{
+		title: "GET INVOLVED",
+		links: [
+			{ linkName: "Need Help?", link: "" },
+			{ linkName: "Advocate", link: "" },
+			{ linkName: "Volunteer", link: "" },
+			{ linkName: "Internships", link: "" },
+			{ linkName: "Careers", link: "" },
+			{ linkName: "Contact us", link: "" },
+		],
+	},
+	{
+		title: "GIVE",
+		links: [
+			{ linkName: "Financial Gifts", link: "" },
+			{ linkName: "Welcome Home Kits", link: "" },
+			{ linkName: "Wishlist Items", link: "" },
+		],
+	},
+];
 
 export default function Footer() {
 	return (
@@ -11,77 +56,44 @@ export default function Footer() {
 			<footer>
 				<div className={styles.container}>
 					<section className={styles.linksContainer}>
+						{footerData.map(linkSection => {
+							return (
+								<div className={styles.linkContainer}>
+									<h3>{linkSection.title}</h3>
+									<div className={styles.links}>
+										{linkSection.links.map(link => {
+											return (
+												<a href={link.link} target="_blank" alt={link.linkName}>
+													{link.linkName}
+												</a>
+											);
+										})}
+									</div>
+								</div>
+							);
+						})}
+
 						<div className={styles.linkContainer}>
-							<h3>WHO WE ARE</h3>
-							<div className={styles.links}>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-							</div>
-						</div>
-						<div className={styles.linkContainer}>
-							<h3>WHO WE ARE</h3>
-							<div className={styles.links}>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-							</div>
-						</div>
-						<div className={styles.linkContainer}>
-							<h3>WHO WE ARE</h3>
-							<div className={styles.links}>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-							</div>
-						</div>
-						<div className={styles.linkContainer}>
-							<h3>WHO WE ARE</h3>
-							<div className={styles.links}>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-								<a href="#" alt="altyalt">
-									Mission & Vision
-								</a>
-							</div>
-						</div>
-						<div className={styles.linkContainer}>
-							<button className={styles.donateButton}>Donate</button>
+							<button className={styles.donateButton}>DONATE</button>
 						</div>
 					</section>
 
-					<section className={styles.container}></section>
+					<section className={styles.legalsContainer}>
+						<div className={styles.legal}>
+							<div className={styles.identity}>
+								<img src={FPLogo} className={styles.lowerLogo} />
+								<div className={styles.copyright}>
+									&copy; Family Promise 2021. Proudly Made with Wix.com
+								</div>
+							</div>
+
+							<div className={styles.officeButtonContainer}>
+								<button className={styles.officeButton}>
+									Family Promise National Office
+								</button>
+							</div>
+						</div>
+					</section>
 				</div>
 			</footer>
 		</div>
