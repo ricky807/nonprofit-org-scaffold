f3754cf6c18050a0c7feed2fbfe49e3222235cac
MichaelHabermas <michaelghabermas@gmail.com>
2021-11-17 08:09:37 -0500

    updated share the promise bgColor

--- src/components/Global/HelpShareThePromise.js
+++ src/components/Global/HelpShareThePromise.js
@@ -1,10 +1,6 @@
 import React from "react";
 
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaYoutube,
-} from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 
 import * as styles from "../../styles/Global/HelpShareThePromise.module.css";
 
@@ -12,19 +8,19 @@ import * as globalStyles from "../../styles/Global/Global.module.css";
 import { FACEBOOK, INSTAGRAM, YOUTUBE } from "../../utils/data/socials";
 
 export default function HelpShareThePromise() {
-  const redirectTo = (link) => window.open(link);
+	const redirectTo = link => window.open(link);
 
-  return (
-    <div className={globalStyles.genericContainer}>
-      <div className={styles.container}>
-        <h1>Help share the promise</h1>
+	return (
+		<div className={styles.sectionContainer}>
+			<div className={styles.container}>
+				<h1>Help Share the Promise</h1>
 
-        <div className={styles.socials}>
-          <FaFacebookF onClick={() => redirectTo(FACEBOOK)} />
-          <FaYoutube onClick={() => redirectTo(YOUTUBE)} />
-          <FaInstagram onClick={() => redirectTo(INSTAGRAM)} />
-        </div>
-      </div>
-    </div>
-  );
+				<div className={styles.socials}>
+					<FaFacebookF onClick={() => redirectTo(FACEBOOK)} />
+					<FaYoutube onClick={() => redirectTo(YOUTUBE)} />
+					<FaInstagram onClick={() => redirectTo(INSTAGRAM)} />
+				</div>
+			</div>
+		</div>
+	);
 }
