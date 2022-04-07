e6078db5417551e24b840f06f6535746a59e54c8
isaiah-j <greatpup1940@gmail.com>
2022-04-07 11:40:28 -0400

    Deleted unused components from Layout folder and mounted Top Navigation

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SocialLinks from "./components/SocialLinks";
+import TopNavigation from './components/TopNavigation'
 
 export default function Index({ children }) {
 	return (
 		<>
+			<TopNavigation />
 			<Navbar />
 			{/* <SocialLinks /> */}
 			<main>{children}</main>
