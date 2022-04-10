6f04914410d3282da096f6b0d3a30e5e4b2211aa
isaiah-j <greatpup1940@gmail.com>
2022-04-10 19:17:20 -0400

    Resizing image to fit on mobile

--- src/pages/core-values.js
+++ src/pages/core-values.js
@@ -10,11 +10,11 @@ export default function coreValues() {
   return (
     <div>
       <Banner backgroundColor={"lightpurple"} textDirection="left">
-        <h1 style={{fontSize: '4rem'}}>Core Values</h1>
+        <h1 style={{ fontSize: "4rem" }}>Core Values</h1>
       </Banner>
 
       <Container>
-        <img src={values} />
+        <img style={{ maxWidth: '100%', maxHeight: '100%', height: 'auto' }} src={values} />
       </Container>
     </div>
   );
