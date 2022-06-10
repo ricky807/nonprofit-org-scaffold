908ff5f05ab3b18ba21e741c86300ddbaf77d275
ricky807 <80001920+ricky807@users.noreply.github.com>
2022-06-09 18:06:05 -0700

    updated nav bar

--- client/src/components/Layout/components/Navbar.jsx
+++ client/src/components/Layout/components/Navbar.jsx
@@ -23,67 +23,35 @@ function Navigation() {
               style={{width: "100%" }}
               navbarScroll
             >
-              <NavDropdown
-                className="ml-auto"
-                title="Who We Are"
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Item href="/mission">
-                  MISSION & VISION
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/core-values">
-                  CORE VALUES
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/history">HISTORY</NavDropdown.Item>
-                <NavDropdown.Item href="/staff">STAFF</NavDropdown.Item>
-                <NavDropdown.Item href="/board">BOARD</NavDropdown.Item>
-                <NavDropdown.Item href="/financials">
-                  FINANCIALS
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/communitypartners">
-                  COMMUNITY PARTNERS
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/stories">STORIES</NavDropdown.Item>
+              <NavDropdown className="ml-auto" title="Who We Are" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="/mission">MISSION & VISION</NavDropdown.Item>
+                <NavDropdown.Item href="/core-values">SERVICES</NavDropdown.Item>
+                <NavDropdown.Item href="/staff">LEADERSHIP</NavDropdown.Item>
               </NavDropdown>
 
-              <NavDropdown title="What We Do" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="/needHelp">NEED HELP?</NavDropdown.Item>
-                <NavDropdown.Item href="/prevent">PREVENT</NavDropdown.Item>
-                <NavDropdown.Item href="/hap">
-                  HOUSING ASSISTANCE
-                </NavDropdown.Item>
-
-                <NavDropdown.Item href="/provide">PROVIDE</NavDropdown.Item>
-
-                <NavDropdown.Item href="/prepare">PREPARE</NavDropdown.Item>
-
-                <NavDropdown.Item href="/covid19response">
-                  COVID 19 RESPONSE
-                </NavDropdown.Item>
+              <NavDropdown className="ml-auto" title="Ministries" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="/core-values">MEN</NavDropdown.Item>
+                <NavDropdown.Item href="/history">WOMEN</NavDropdown.Item>
+                <NavDropdown.Item href="/staff">YOUTH</NavDropdown.Item>
+                <NavDropdown.Item href="/staff">OUTREACH</NavDropdown.Item>
               </NavDropdown>
 
-              <NavDropdown title="Ways To Give" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="/goods-items">
-                  GOODS & ITEMS
-                </NavDropdown.Item>
+              <Nav.Link href="/donate">Events</Nav.Link>
+
+              <Nav.Link href="/donate">Sermons</Nav.Link>
+
+              <Nav.Link href="/donate">Give</Nav.Link>
+              {/*
+              <NavDropdown title="Ways to Give" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="/goods-items">GOODS & ITEMS</NavDropdown.Item>
                 <NavDropdown.Item href="/learn">LEARN MORE</NavDropdown.Item>
                 <NavDropdown.Item href="/volunteer">VOLUNTEER</NavDropdown.Item>
-
-                <NavDropdown.Item href="/internships">
-                  INTERNSHIPS
-                </NavDropdown.Item>
+                <NavDropdown.Item href="/internships">INTERNSHIPS</NavDropdown.Item>
                 <NavDropdown.Item href="/careers">CAREERS</NavDropdown.Item>
               </NavDropdown>
 
-              <NavDropdown title="Events" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="/ungala">
-                  UnGala 2022
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/nightwithoutabed">
-                  Night w/o A Bed
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/donate">DONATE</Nav.Link>
+              <Nav.Link href="/donate">DONATE</Nav.Link>*/}
+
             </Nav>
           </Navbar.Collapse>
         </Container>
